227a317c7e39bc60da77a6c5f57937ab3cceae78
Jestzer <james@buonopane.email>
2024-07-08 15:22:51 -0400

    Avalonia base.

---
+++ LMFOOLS/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace LMFOOLS.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ LMFOOLS/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace LMFOOLS.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
