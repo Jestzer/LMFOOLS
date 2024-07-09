f9c79ec1bc6b6ee6687e31bb55fdd73291f80b15
Jestzer <james@buonopane.email>
2024-07-09 16:29:41 -0400

    Submitting things while they still work.

---
+++ LMFOOLS/Settings.cs
@@ -0,0 +1,7 @@
+namespace LMFOOLS;
+
+public class Settings
+{
+    public string LicenseFilePathSetting { get; set; } = string.Empty;
+    public string LmgrdPathSetting { get; set; } = string.Empty;
+}
