ba8857096bc67df80bc45cbf58fc44e1485940ee
Jestzer <126624248+Jestzer@users.noreply.github.com>
2024-07-09 21:49:00 -0400

    Technically works fine. Needs polish and more features. Good enough for now.

--- LMFOOLS/Settings.cs
+++ LMFOOLS/Settings.cs
@@ -4,4 +4,5 @@ public class Settings
 {
     public string LicenseFilePathSetting { get; set; } = string.Empty;
     public string LmgrdPathSetting { get; set; } = string.Empty;
+    public string LmutilPathSetting { get; set; } = string.Empty;
 }
